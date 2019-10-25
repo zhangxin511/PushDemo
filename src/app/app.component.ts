b35ec881ac31a317e9f92df1d92f43e4fbd3feff
Xin Zhang <xzhang@kabbage.com>
2019-10-24 23:54:20 -0400

    Sprint 2, hook up Angular service worker with Angular fire and FCM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/messaging';
+import { environment } from 'src/environments/environment';
+import { SwUpdate, SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  template: `
+  <button (click)="permitToNotify()">
+    Click button to Click Notifiy
+  </button>
+  <br/>
+  Device token: {{displayToken}}
+  `
 })
 export class AppComponent {
-  title = 'PushDemo';
+  displayToken: string;
+  constructor(updates: SwUpdate, push: SwPush) {
+    updates.available.subscribe(_ => updates.activateUpdate().then(() => {
+      console.log('reload for update');
+      document.location.reload();
+    }));
+    push.messages.subscribe(msg => console.log('push message', msg));
+    push.notificationClicks.subscribe(click => console.log('notification click', click));
+    if (!firebase.apps.length) {
+      firebase.initializeApp(environment.firebase);
+      navigator.serviceWorker.getRegistration().then(swr => firebase.messaging().useServiceWorker(swr));
+    }
+  }
+
+  permitToNotify() {
+    const messaging = firebase.messaging();
+    messaging.requestPermission()
+      .then(() => messaging.getToken().then(token => this.displayToken = token))
+      .catch(err => {
+        console.log('Unable to get permission to notify.', err);
+      });
+  }
 }
